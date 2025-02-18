64cf602675eaa03d19a69e0822721d371c09e17e
Bibek Mainali <mainalibibek01@gmail.com>
2025-02-19 01:55:11 +0545

    created a predict page that gives the chart of the stock selected.

--- prediction/urls.py
+++ prediction/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about, name='about'),
+    path('', views.main, name='main'),
+    path('predict/',views.predict, name='predict'),
+    path('about/',views.about, name='about'),
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponseBadRequest
 #rendering about page
 def about(request):
     return render(request, 'prediction/about.html')
+
+def main(request):
+    return render(request,'prediction/main.html')
+
+def predict(request):
+    return render(request,'prediction/predict.html')
